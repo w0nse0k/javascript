13ede40411aea772cd8909aa6ad2f55d0352881c
Jacob <w0nse0k@naver.com>
2026-09-16 01:53:54 +0900

    arrays/interation에 fiter(), reduce() 추가

--- src/tutorial/arrays/iterations.js
+++ src/tutorial/arrays/iterations.js
@@ -31,6 +31,14 @@ console.log(newCars); // ["<li>BMW</li>", "<li>Volvo</li>", "<li>Mini</li>"]
 text = `<ul>${newCars.join("")}</ul>`;
 console.log(text);
 
-// fiter()
+// fiter(): 배열에서 특정 조건을 만족하는 원소들만으로 이루어진 배열
+const numbers = [45, 4, 9, 16, 25];
+const over18 = numbers.filter((elem) => elem > 18);
+console.log(over18);
 
-// reduce()
+// reduce(): 배열을 한 개의 값으로 줄임
+const total = numbers.reduce((acc, elem) => acc + elem, 0);
+console.log(total);
+
+const carsList = cars.reduce((acc, elem) => acc + `<li>${elem}</li>`, "");
+console.log(`<ul>${carsList}</ul>`);
